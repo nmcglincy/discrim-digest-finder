3422c95f1373da53c612c23580fa12c89c2a953c
Nicholas McGlincy <nmcglincy@gmail.com>
2014-09-03 17:47:44 -0700

    process and clean up output files

--- emboss-commands.sh
+++ emboss-commands.sh
@@ -1,9 +1,15 @@
-# Playing around with different EMBOSS commands to give the output I want
 # 
 # Presumes that:
 # 	EMBOSS is present and in the PATH
 #	this file has be made executable by chmod u+x emboss-commands.sh
 # 
-restrict -fragments -sequence X65923.embl
-# this requires some commands, can I make them sensible defaults?
-restrict -sequence N5.fasta -max 10 -solofragment 
+# do restrict on it
+for i in $@
+	do
+	restrict -sequence $i -sitelen 4 -enzymes all -max 4 -solofragment -blunt -ambiguity N -plasmid Y -limit Y -alphabetic Y -auto Y
+	done
+# clean up output files
+for a in $(ls *.restrict)
+	do
+		sed '/^$/d;/^#/d' $a > "newfile-$a"
+	done
